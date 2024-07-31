78f459776c1e9c284dd0121679f26e8b105d4a8c
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-07-31 08:11:24 +0300

    add Property Injection

---
+++ Injectify/Injectify.Package/Attributes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Injectify.Package
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method , AllowMultiple =false)]
+    public class InjectAttribute : Attribute { }
+
+
+}
---
+++ Injectify/Injectify.Package/Inject/InjectHelper.cs
@@ -0,0 +1,35 @@
+﻿using Injectify.Package.ContainerBuilder;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Injectify.Package.Inject
+{
+    public static class InjectHelper
+    {
+
+        public static void InjectProperties( Container container,  object instance  )
+        {
+            var type = instance.GetType();
+            IEnumerable<PropertyInfo> properties = type.GetProperties()
+                                                         .Where(propInfo => propInfo.CanWrite
+                                                             && propInfo.GetCustomAttribute<InjectAttribute>() != null);
+
+            properties.ToList().ForEach(propInfo =>
+            {
+                var value = container.Resolve(propInfo.PropertyType);
+
+                propInfo.SetValue(instance,value);
+            });
+        }
+
+       
+
+
+
+    }
+}
