6151684d99564115fab8f173749e5482c3de3057
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-07-31 08:17:23 +0300

    Test Method Injection

--- Injectify/TestProject/Program.cs
+++ Injectify/TestProject/Program.cs
@@ -7,66 +7,73 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
-            #region Basic Registration and Resolution
-            var conatiner = new Container();
-            conatiner.Register<IService, Service>();
-            var serviceObj = conatiner.Resolve<IService>();
-            serviceObj.DoSomething();
+            //#region Basic Registration and Resolution
+            //var conatiner = new Container();
+            //conatiner.Register<IService, Service>();
+            //var serviceObj = conatiner.Resolve<IService>();
+            //serviceObj.DoSomething();
+            //#endregion
+
+            //#region  Lifetime Management 
+            //var conatiner = new Container();
+
+            //Singleton
+            //conatiner.Register<IService, Service>(LifeTime.Singleton);
+            //var service1 = conatiner.Resolve<IService>();
+            //var service2 = conatiner.Resolve<IService>();
+            //if true that mean ref in the same location that mean only object create
+            //Console.WriteLine(object.ReferenceEquals(service1, service2));
+
+            //Transient
+            //conatiner.Register<IService, Service>(LifeTime.Transient);
+            //var service1 = conatiner.Resolve<IService>();
+            //var service2 = conatiner.Resolve(typeof(IService));
+            ////  if false that mean ref are created each time requested !
+            //Console.WriteLine(object.ReferenceEquals(service1, service2));
+
+
+            //// Scoped 
+            //conatiner.Register<IService, Service>(LifeTime.Scoped);
+            //IService service1;
+            //using (var scope1 = conatiner.CreateScope())
+            //{
+            //    service1 = scope1.Resolve<IService>();
+            //    var service2 = scope1.Resolve<IService>();
+
+            //    //  should be true beacuse it deals as a singleton in the same scope 
+
+            //    Console.WriteLine(object.ReferenceEquals(service1, service2));// Outputs: True
+            //}
+
+            //using (var scope2 = conatiner.CreateScope())
+            //{
+            //    var service3 = scope2.Resolve<IService>();
+            //    //  should be False beacuse it deals as a Transient in the defferent scope 
+            //    Console.WriteLine(object.ReferenceEquals(service1, service3)); // Outputs: False
+            //}
+            //#endregion
+            //#region Property Injection 
+
+
+
+            //var conatiner = new Container();
+            //conatiner.Register<IService, Service>();
+            //conatiner.Register<Client, Client>();
+            //var client = conatiner.Resolve<Client>();
+            //Console.WriteLine(client.Service != null);
+            //#endregion
+
+
+
+            #region Method Injection 
+
+            var container = new Container();
+            container.Register<IService, Service>();
+            container.Register<Client2, Client2>();
+            var client = container.Resolve<Client2>();
+
+
             #endregion
-
-            #region  Lifetime Management 
-            var conatiner = new Container();
-
-            Singleton
-            conatiner.Register<IService, Service>(LifeTime.Singleton);
-            var service1 = conatiner.Resolve<IService>();
-            var service2 = conatiner.Resolve<IService>();
-            if true that mean ref in the same location that mean only object create
-            Console.WriteLine(object.ReferenceEquals(service1, service2));
-
-            Transient
-            conatiner.Register<IService, Service>(LifeTime.Transient);
-            var service1 = conatiner.Resolve<IService>();
-            var service2 = conatiner.Resolve(typeof(IService));
-            //  if false that mean ref are created each time requested !
-            Console.WriteLine(object.ReferenceEquals(service1, service2));
-
-
-            // Scoped 
-            conatiner.Register<IService, Service>(LifeTime.Scoped);
-            IService service1;
-            using (var scope1 = conatiner.CreateScope())
-            {
-                service1 = scope1.Resolve<IService>();
-                var service2 = scope1.Resolve<IService>();
-
-                //  should be true beacuse it deals as a singleton in the same scope 
-
-                Console.WriteLine(object.ReferenceEquals(service1, service2));// Outputs: True
-            }
-
-            using (var scope2 = conatiner.CreateScope())
-            {
-                var service3 = scope2.Resolve<IService>();
-                //  should be False beacuse it deals as a Transient in the defferent scope 
-                Console.WriteLine(object.ReferenceEquals(service1, service3)); // Outputs: False
-            }
-            #endregion
-            #region Property Injection 
-
-
-
-            var conatiner = new Container();
-            conatiner.Register<IService, Service>();
-            conatiner.Register<Client, Client>();
-            var client = conatiner.Resolve<Client>();
-            Console.WriteLine(client.Service != null);
-
-
-
-
-            #endregion
-
 
 
 
@@ -93,10 +100,21 @@ namespace TestProject
     {
         [Inject]
         public IService Service { get; set; }
-
-
-
-
     }
+
+    // Test Method Injection 
+    public class Client2
+    {
+        private IService _service;
+
+        [Inject]
+        public void Initialize(IService service)
+        {
+            _service = service;
+            Console.WriteLine(_service != null );
+        }
+    }
+
+
 
 }
