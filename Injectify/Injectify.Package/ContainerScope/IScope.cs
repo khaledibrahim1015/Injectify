9bf9ac6cce4dd3837729b7ba67dd56cf208790ad
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-07-31 06:41:21 +0300

    add Scoped Lifetime

---
+++ Injectify/Injectify.Package/ContainerScope/IScope.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Injectify.Package.ContainerScope
+{
+    public interface IScope : IDisposable
+    {
+        object Resolve(Type serviceType);
+        T Resolve<T>();
+
+    }
+}
---
+++ Injectify/Injectify.Package/ContainerScope/Scope.cs
@@ -0,0 +1,45 @@
+﻿using Injectify.Package.Container;
+using Injectify.Package.Enums;
+using System.Collections.Concurrent;
+namespace Injectify.Package.ContainerScope
+{
+    public class Scope : IScope
+    {
+        private readonly ConcurrentDictionary<Type , object > _scopedInstances = new ConcurrentDictionary<Type , object >();    
+
+        private readonly Injectify.Package.Container.Container _container;
+        public Scope(Injectify.Package.Container.Container container)
+        {
+            _container = container;
+        }
+
+
+        public T Resolve <T> ()
+            => (T) Resolve(typeof(T));
+
+
+        public object Resolve(Type serviceType)
+        {
+            if(_container._registrations.TryGetValue(serviceType, out var registration))
+            {
+                if (registration.LifeTime == LifeTime.Scoped)
+                    return _scopedInstances.GetOrAdd(serviceType, _ => _container.CreateInstance(registration.ImplementationType));
+            }
+            return registration.GetInstance(_container.CreateInstance);
+
+        }
+
+
+
+        public void Dispose()
+        {
+            foreach (var instance in _scopedInstances.Values)
+            {
+                (instance as IDisposable)?.Dispose();
+            }
+            _scopedInstances.Clear();
+        }
+
+
+    }
+}
