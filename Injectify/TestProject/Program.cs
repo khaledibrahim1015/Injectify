105b9419534fd21bfc1c79a4917c0e1b15ab92e7
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-07-31 04:36:18 +0300

    Test Basic Registration and Resolution

---
+++ Injectify/TestProject/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Injectify.Package.Container;
+
+namespace TestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Basic Registration and Resolution
+            var conatiner = new Container();
+            conatiner.Register<IService, Service>();
+            var serviceObj = conatiner.Resolve<IService>();
+            serviceObj.DoSomething();
+            #endregion
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IService
+    {
+        void DoSomething();
+    }
+
+    public class Service : IService
+    {
+        public void DoSomething()
+            => Console.WriteLine("Do Something");
+    }
+
+
+}
